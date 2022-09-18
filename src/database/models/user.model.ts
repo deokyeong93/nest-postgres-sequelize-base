d165b8046a555ec01ebe571db5ebaf6cdfc34599
deokyeong93 <66895208+deokyeong93@users.noreply.github.com>
2022-09-18 18:52:01 +0900

    유효성 검사 연습

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,4 +1,11 @@
-import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  HasMany,
+  NotNull,
+} from 'sequelize-typescript';
 import { Post } from './post.model';
 
 @Table({
@@ -14,18 +21,31 @@ export class User extends Model {
   @Column({
     type: DataType.STRING,
     allowNull: false,
+    validate: {
+      notNull: { msg: '유저는 이름을 반드시 가져야합니다.' },
+      notEmpty: { msg: '이름은 빈 값이 될 수 없습니다.' },
+    },
   })
   name: string;
 
   @Column({
     type: DataType.STRING,
     allowNull: false,
+    validate: {
+      notNull: { msg: '유저는 이메일을 반드시 가져야합니다.' },
+      notEmpty: { msg: '이메일은 빈 값이 될 수 없습니다.' },
+      isEmail: { msg: '올바른 이메일 유형을 따르세요' },
+    },
   })
   email: number;
 
   @Column({
     type: DataType.STRING,
     allowNull: false,
+    validate: {
+      notNull: { msg: '유저는 역할을 반드시 가져야합니다.' },
+      notEmpty: { msg: '역할은 빈 값이 될 수 없습니다.' },
+    },
   })
   role: string;
 
